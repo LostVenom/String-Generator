ca438c0d55c9347e3e7c8c3ded8e2cf1b38e9e6b
PIRATE 303 <149289075+PIRATE303@users.noreply.github.com>
2024-01-14 14:28:06 +0530

    Changed Stats Text

--- StringGenerator/bot_users.py
+++ StringGenerator/bot_users.py
@@ -2,7 +2,7 @@ from pyrogram.types import Message
 from pyrogram import Client, filters
 
 from config import OWNER_ID
-from StringGenBot.db.users import add_served_user, get_served_users
+from StringGenerator.db.users import add_served_user, get_served_users
 
 
 @Client.on_message(filters.private & ~filters.service, group=1)
@@ -13,4 +13,4 @@ async def users_sql(_, msg: Message):
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = len(await get_served_users())
-    await msg.reply_text(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ sᴛʀɪɴɢ ɢᴇɴ ʙᴏᴛ :\n\n {users} ᴜsᴇʀs", quote=True)
+    await msg.reply_text(f"⧉ Current Stats:\n\n {users} ᴜsᴇʀs", quote=True)
